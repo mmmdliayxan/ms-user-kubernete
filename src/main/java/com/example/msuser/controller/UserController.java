94de609e78ca701fcdd1a9dd50b4fc23a7c0ade0
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2025-07-29 21:17:18 +0400

    new repo is created

---
+++ src/main/java/com/example/msuser/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.msuser.controller;
+
+
+import com.example.msuser.entity.User;
+import com.example.msuser.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+}
---
+++ src/main/java/com/example/msuser/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.msuser.service;
+
+import com.example.msuser.entity.User;
+import com.example.msuser.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
